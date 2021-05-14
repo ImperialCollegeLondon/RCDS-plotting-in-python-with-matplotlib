1b8e5ef716c00eebefd88d6c3bfbeb9dff2e5fff
coolernato <cmcooling@gmail.com>
2021-05-14 15:41:41 +0100

    Importing files

---
+++ py_file_example.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import math
+
+x = []
+y = []
+
+for i in range(1000):
+  x.append(i * 100)
+  y.append(math.sin(i * 0.01))
+
+plt.plot(x,y)
+
+# This command opens a new window showing the plot
+plt.show()
